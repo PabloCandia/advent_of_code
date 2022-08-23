259d8c28d13b95fe4236ec6d1fa67b1dfe70814e
PabloCandia <111370425+PabloCandia@users.noreply.github.com>
2022-08-23 10:44:28 +0100

    Add files via upload

--- python/2020/day_3/2020-3.py
+++ python/2020/day_3/2020-3.py
@@ -1,17 +1,24 @@
-def count_trees(filename, start_point, right, down):
-    c = 0
+def read_file(filename):
     with open(filename,"r") as document:
-        docu_list = list(document)
-        len_str = len(docu_list[0])-1
-        for i in range((len(docu_list))//down+(len(docu_list))%down):
-            if docu_list[down*i][(start_point+i*right)%len_str]=="#":
-                c += 1
-        return c    
+        doc = [line.strip("\n") for line in document]
+    return doc
+
+def get_trajectory(start_point,step,len_chart):
+    trajectory = [start_point]
+    for coords in trajectory:
+        if coords[1]+step[1] >= len_chart:
+            break
+        trajectory.append([coords[0]+step[0], coords[1]+step[1]])
+    return trajectory
+    
+def count_trees(filename, start_point, step):
+    chart = read_file(filename)
+    return sum([int(chart[coord[1]][coord[0]%(len(chart[0]))]=="#") for coord in get_trajectory(start_point, step, len(chart))])
 
 if __name__ == "__main__":
     print("Enter filename:")
     filename = input()
-    print("The answer to part one is "+str(count_trees(filename,0,3,1))+" trees.")
+    print("The answer to part one is "+str(count_trees(filename, [0,0], [3,1]))+" trees.")
     instructions = [[1,1],[3,1],[5,1],[7,1],[1,2]]
-    product = eval("*".join([str(count_trees(filename,0,instruction[0],instruction[1])) for instruction in instructions]))
+    product = eval("*".join([str(count_trees(filename, [0,0], instruction)) for instruction in instructions]))
     print("The answer to part two is "+str(product)+".")
